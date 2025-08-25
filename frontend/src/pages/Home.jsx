f1777aed0f3fa37126767bf4684bf97bccb3eb30
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-25 14:54:19 +0530

    added css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>
+    <div className='min-h-screen'>
       <Navbar />
+      <Header />
     </div>
   )
 }
