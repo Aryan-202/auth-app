524f703c3489694c1b78d75e930211853052855b
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-25 19:43:24 +0530

    made some changes in backend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, use, useState } from "react";
+import { toast } from "react-toastify";
 
 export const AppContext = createContext();
 
@@ -6,8 +7,17 @@ export const AppContextProvider = (props) => {
     
     const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
     const [isLoggedin, setIsLoggedin] = useState(false);
-    const [userData, setUserData] = useState(null);
-    
+    const [userData, setUserData] = useState(false);
+
+    const getAuthState = async () => {
+        try {
+            const { data } = await axios.get(`${backendUrl}/api/auth/-s-auth`, { withCredentials: true });
+            setIsLoggedin(data.isLoggedin);
+            setUserData(data.user || false);
+        } catch (error) {
+            toast.error(error.message || 'Failed to fetch auth state');
+        }
+    }
 
 
     const value = {
