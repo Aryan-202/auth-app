ad17e2fc56f2e243a0b85fa8248b0604add12c7f
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-08-25 11:57:08 +0530

    made user router

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 // declarations
 const app = express();
@@ -22,6 +23,7 @@ app.get('/', (req, res)=>{
 });
 
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
